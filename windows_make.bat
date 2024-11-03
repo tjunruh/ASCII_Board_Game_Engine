d9db9e4692881955faa87b5dd0fccb2cc98b7657
tjunruh <tanner.unruh@gmail.com>
2024-11-02 22:45:41 -0500

    Package header files for windows build and include file manager

--- windows_make.bat
+++ windows_make.bat
@@ -2,10 +2,14 @@ msbuild /m /p:Configuration=Release Code/ascii_engine/ascii_engine.vcxproj
 msbuild /m /p:Configuration=Release Code/build_board_config/build_board_config.vcxproj
 msbuild /m /p:Configuration=Release Code/validate_board_config/validate_board_config.vcxproj
 mkdir build
+mkdir build\headers
+mkdir build\headers\ascii_engine
 copy Code\ascii_engine\Release\ascii_engine.dll build
 copy Code\ascii_engine\Release\ascii_engine.lib build
 copy Code\build_board_config\Release\build_board_config.exe build
 copy Code\validate_board_config\Release\validate_board_config.exe build
+copy Code\ascii_engine\*.h build\headers\ascii_engine
+copy Code\file_manager\*.h build\headers\ascii_engine
 rd /s /q Code\ascii_engine\Release
 rd /s /q Code\build_board_config\Release
 rd /s /q Code\validate_board_config\Release
