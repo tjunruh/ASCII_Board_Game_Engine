541c7511597c50d8ec328540f21420ffcea93a8d
tjunruh <tanner.unruh@gmail.com>
2025-02-09 00:03:08 -0600

    Create set cursor line demo for menu

---
+++ examples/menu_demo-set_cursor_line/menu_demo-set_cursor_line.cpp
@@ -0,0 +1,71 @@
+#include <ascii_io.h>
+#include <frame.h>
+#include <menu.h>
+#include <spacer.h>
+#include <label.h>
+#include <string>
+
+int main()
+{
+	ascii_io::ascii_engine_init();
+
+	// - widgets are placed on the frame in the order that they are created
+	// - if neither "merge" nor "new line" are passed as a parameter, the widget will be placed on the same line and to the right of the other widgets in the line
+	// - "merge" will place the widget below the preceding widget while still being in the same line
+	// - "new line" will place the widget below on a new line
+	frame* my_frame = new frame();
+	spacer left_spacer(my_frame);
+	menu my_menu(my_frame);
+	spacer right_spacer(my_frame);
+	label my_label(my_frame, "new line");
+
+	left_spacer.set_width_multiplier(0.5); // reduce spacer width since menu will take up more width with labels included
+	right_spacer.set_width_multiplier(0.5);
+
+	// uncomment below to enable line drawing
+	//my_frame->enable_dec(true);
+
+	my_menu.add_border(true);
+	my_menu.set_spacing(0, 0, 0, 1);
+	for (unsigned int i = 0; i < 10; i++)
+	{
+		my_menu.append_item(std::to_string(9 - i));
+		my_menu.set_item_label(std::to_string(9 - i), "line " + std::to_string(i) + "    Select to jump to line " + std::to_string(9 - i)); // Add label for item
+	}
+	my_menu.append_item("Exit");
+	my_menu.enable_quit(); // can exit menu with 'q' without selecting item
+	my_menu.set_alignment("center");
+	my_menu.sync();
+
+	my_label.add_border(true);
+	my_label.set_alignment("center");
+	my_label.set_output("Select an Item.");
+
+	ascii_io::hide_cursor();
+	my_menu.sync();
+
+	my_frame->display();
+	while (1)
+	{
+		std::string selection = "";
+		int key = ascii_io::undefined;
+		my_menu.get_selection(selection, key);
+		my_menu.sync();
+		if (selection == "Exit")
+		{
+			break;
+		}
+		else
+		{
+			my_label.set_output(selection + " was selected.");
+			my_label.refresh();
+			my_menu.set_cursor_line(stoi(selection));
+		}
+
+	}
+
+	ascii_io::show_cursor();
+	
+	delete(my_frame);
+	ascii_io::ascii_engine_end();
+}
---
+++ examples/menu_demo-set_cursor_line/windows_make.bat
@@ -0,0 +1,5 @@
+msbuild /m /p:Configuration=Release windows_msbuild/menu_demo-set_cursor_line.vcxproj
+mkdir build
+copy windows_msbuild\Release\menu_demo-set_cursor_line.exe build\
+copy ..\..\build\ascii_engine.dll build\
+FOR /D %%p IN ("windows_msbuild\*.*") DO rmdir "%%p" /s /q
