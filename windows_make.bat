908d909710fd1570cabaf04220cc5799dc36d8e7
tjunruh <tanner.unruh@gmail.com>
2025-03-22 17:06:35 -0500

    Trigger failure if windows build fails

--- windows_make.bat
+++ windows_make.bat
@@ -1,6 +1,16 @@
+status=0
 msbuild /m /p:Configuration=Release Code/ascii_engine/windows_msbuild/ascii_engine.vcxproj
+IF %ERRORLEVEL% NEQ 0 (
+  set status=1
+)
 msbuild /m /p:Configuration=Release Code/build_board_config/windows_msbuild/build_board_config.vcxproj
+IF %ERRORLEVEL% NEQ 0 (
+  set status=1
+)
 msbuild /m /p:Configuration=Release Code/validate_board_config/windows_msbuild/validate_board_config.vcxproj
+IF %ERRORLEVEL% NEQ 0 (
+  set status=1
+)
 mkdir build
 mkdir build\headers
 mkdir build\headers\ascii_engine
@@ -13,3 +23,5 @@ copy Code\file_manager\*.h build\headers\ascii_engine
 FOR /D %%p IN ("Code\ascii_engine\windows_msbuild\*.*") DO rmdir "%%p" /s /q
 FOR /D %%p IN ("Code\build_board_config\windows_msbuild\*.*") DO rmdir "%%p" /s /q
 FOR /D %%p IN ("Code\validate_board_config\windows_msbuild\*.*") DO rmdir "%%p" /s /q
+
+EXIT /b %status%
