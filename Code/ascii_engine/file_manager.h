067b76d6ed12e914bb9daa0e9ff08d8c273388ce
Matthew Malinowski <mateuszm@matthewm.com.pl>
2026-08-08 22:48:17 -0500

    Transfer ascii_engine to cmake

---
+++ Code/ascii_engine/file_manager.h
@@ -0,0 +1,7 @@
+#ifndef LIBASCII_ENGINE_ENGINE_FILE_MANAGER
+#define LIBASCII_ENGINE_ENGINE_FILE_MANAGER
+
+// Allow backwards comptability with Makefile version of the library
+#include <file_manager.h>
+
+#endif
